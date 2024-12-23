8790524eba9ef8ad0d5cce33db107f5204cfacf4
PabloTwoElf <pablo.escoba2005@gmail.com>
2024-12-22 20:23:49 -0500

    Agregar archivos de proyecto.

---
+++ ViewModels/PCAEAboutViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Notes.ViewModels
+{
+    internal class PCAEAboutViewModel
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "Esta app es creada por PABLO CRIOLLO ESCOBAR ";
+        public ICommand ShowMoreInfoCommand { get; }
+
+        public PCAEAboutViewModel()
+        {
+            ShowMoreInfoCommand = new AsyncRelayCommand(ShowMoreInfo);
+        }
+
+
+        async Task ShowMoreInfo() =>
+            await Launcher.Default.OpenAsync(MoreInfoUrl);
+    }
+}
---
+++ ViewModels/PCAENotesViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Notes.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Notes.ViewModels;
+
+internal class PCAENotesViewModel : IQueryAttributable
+{
+    public ObservableCollection<ViewModels.PCAENoteViewModel> AllNotes { get; }
+    public ICommand NewCommand { get; }
+    public ICommand SelectNoteCommand { get; }
+
+    public PCAENotesViewModel()
+    {
+        AllNotes = new ObservableCollection<ViewModels.PCAENoteViewModel>(Models.Note.LoadAll().Select(n => new PCAENoteViewModel(n)));
+        NewCommand = new AsyncRelayCommand(NewNoteAsync);
+        SelectNoteCommand = new AsyncRelayCommand<ViewModels.PCAENoteViewModel>(SelectNoteAsync);
+    }
+
+    private async Task NewNoteAsync()
+    {
+        await Shell.Current.GoToAsync(nameof(Views.NotePage));
+    }
+
+    private async Task SelectNoteAsync(ViewModels.PCAENoteViewModel note)
+    {
+        if (note != null)
+            await Shell.Current.GoToAsync($"{nameof(Views.NotePage)}?load={note.Identifier}");
+    }
+
+    void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.ContainsKey("deleted"))
+        {
+            string noteId = query["deleted"].ToString();
+            PCAENoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+            // If note exists, delete it
+            if (matchedNote != null)
+                AllNotes.Remove(matchedNote);
+        }
+        else if (query.ContainsKey("saved"))
+        {
+            string noteId = query["saved"].ToString();
+            PCAENoteViewModel matchedNote = AllNotes.Where((n) => n.Identifier == noteId).FirstOrDefault();
+
+            // If note is found, update it
+            if (matchedNote != null)
+                matchedNote.Reload();
+
+            // If note isn't found, it's new; add it.
+            else
+                AllNotes.Add(new PCAENoteViewModel(Note.Load(noteId)));
+        }
+    }
+}
